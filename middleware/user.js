46e12300cb240028c2e75d2f46f9847ed5591a44
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-08 22:31:34 +0530

    added all middleware logic

---
+++ middleware/user.js
@@ -0,0 +1,7 @@
+// Middleware for handling auth
+function userMiddleware(req, res){
+    // Implement user auth logic
+    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+}
+
+module.exports = userMiddleware;
