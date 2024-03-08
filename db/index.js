4cd6f1bf69541f7379879d0dc47374cb61034b4c
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-08 22:41:40 +0530

    added admin,user and course schema in db

--- db/index.js
+++ db/index.js
@@ -5,14 +5,26 @@ mongoose.connect('');
 // Schema definition here
 const AdminSchema = new mongoose.Schema({
     // Admin Schema here
+    username: String,
+    password: String
 })
 
 const UserSchema = new mongoose.Schema({
     // User Schem here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Courses'
+    }]
 })
 
 const CourseSchema = new mongoose.Schema({
     // Course Schema here
+   title: String,
+   description: String,
+   imageLink: String,
+   price: Number
 })
 
 const Admin = mongoose.model('Admin', AdminSchema);
