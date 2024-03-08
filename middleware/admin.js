a352ae15c6b6e77cb227206b2a6e6d3407db7f8e
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-08 22:57:32 +0530

    added user authentication using jwt

--- middleware/admin.js
+++ middleware/admin.js
@@ -6,7 +6,7 @@ function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
     const token = req.headers.authorization;
-    const words = token.split(" ");
+    const words = token.split(" "); // ["Bearer", "token"]
     const jwtToken = words[1];
     try{
         const decodedValue = jwt.verify(token, JWT_SECRET);
--- middleware/user.js
+++ middleware/user.js
@@ -1,7 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
 // Middleware for handling auth
 function userMiddleware(req, res){
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+    const decodedValue = jwt.verify(token, JWT_SECRET);
+
+    if(decodedValue.username){
+        req.username = decodedValue.username;
+        req.randomData = "Asdasdasdasd";
+        next();
+    } else{
+        res.sendStatus(403).json({
+            msg: "You are not authenticated"
+        })
+    }
 }
 
 module.exports = userMiddleware;
