c623f856f259e3dffb0fa4ebc2b1b697882fe2c3
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-08 22:24:10 +0530

    added initial schema layout

--- db/index.js
+++ db/index.js
@@ -7,3 +7,20 @@ const AdminSchema = new mongoose.Schema({
     // Admin Schema here
 })
 
+const UserSchema = new mongoose.Schema({
+    // User Schem here
+})
+
+const CourseSchema = new mongoose.Schema({
+    // Course Schema here
+})
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
