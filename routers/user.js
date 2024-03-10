c3c8df02a3c0e12d84e7ad3b7fc845af7fe91b23
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-10 19:15:52 +0530

    included user signup and see all courses logic

--- routers/user.js
+++ routers/user.js
@@ -1,18 +1,33 @@
 const { Router } = require("express");
 const router = Router();
+const { User, Course} = require("../db/index");
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username,
+        password
+    })
+    res.json({
+        message: "User created succesfully"
+    })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const allCourses = await Course.find({});
+    res.json({
+        course: allCourses
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
